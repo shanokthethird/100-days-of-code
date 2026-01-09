9ab6ccfa8925f200960efb35e6a8faf1dd159e89
shanokthethird <xxshanok3xx@gmail.com>
2026-01-09 15:27:37 -0300

    FURTHER UPDATES with jinja templates

--- Day_59_BLOG_capstone_project/startbootstrap-clean-blog-gh-pages/main.py
+++ Day_59_BLOG_capstone_project/startbootstrap-clean-blog-gh-pages/main.py
@@ -1,12 +1,27 @@
 from flask import Flask, render_template, url_for
+import requests
 
 app = Flask(__name__)
+
+blog_url = "https://api.npoint.io/c790b4d5cab58020d391"
+all_posts = requests.get(blog_url).json()
 
 @app.route('/')
 def get_all_posts():
     return render_template("index.html")
 
-def
+@app.route('/about')
+def about():
+    return render_template("about.html")
+
+@app.route('/contact')
+def contact():
+    return render_template("contact.html")
+
+@app.route('/post/<int:index>')
+def show_post(index):
+    return render_template("post.html", post_id=index)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
